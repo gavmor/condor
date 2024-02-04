9fac2b8e9f0573e1d284ff20fbdbc3962e18c94a
Gavin Morgan <morgan.gavin@gmail.com>
2024-02-04 10:31:29 -0800

    astradb

--- index.ts
+++ index.ts
@@ -2,52 +2,56 @@ import fs from "node:fs/promises";
 import {
   Document,
   VectorStoreIndex,
-  QdrantVectorStore,
-  Ollama,
   serviceContextFromDefaults,
-  serviceContextFromServiceContext,
-  similarity,
+  AstraDBVectorStore,
+  storageContextFromDefaults,
+  SimilarityPostprocessor,
+  NodeWithScore
 } from "llamaindex";
+
+class MySimilarity extends SimilarityPostprocessor {
+  postprocessNodes(nodes: NodeWithScore[]) {
+    console.log(nodes)
+    return super.postprocessNodes(nodes);
+  }
+}
 
 async function main() {
   const path = "slack.txt";
-  const essay = await fs.readFile(path, "utf-8")
-  ;
-  const llm = new Ollama({
-    model: "mistral",
-  });
-  const vectorStore = new QdrantVectorStore({
-    collectionName: "slack",
-    url: "http://localhost:6333",
+  const essay = await fs.readFile(path, "utf-8");
+  // const llm = new Ollama({ model: "mistral", });
+
+  const astraCondor = new AstraDBVectorStore({
+    params: {
+      token: process.env.ASTRA_DB_APPLICATION_TOKEN as string,
+      endpoint: process.env.ASTRA_DB_API_ENDPOINT as string
+    },
   });
 
+  await astraCondor.connect("condor")
   const documents = essay.split("\n").map(text => new Document({ text, id_: path }))
 
-  const index = await VectorStoreIndex.fromDocuments(documents, {
-    vectorStore,
-    serviceContext: serviceContextFromDefaults({
-      llm: llm,
-      embedModel: llm,
-      // chunkOverlap: 25,
-      // chunkSize: 50,
-    }),
-  });
+  // const index = await VectorStoreIndex.fromDocuments(documents, {
+  //   storageContext: await storageContextFromDefaults({ vectorStore: astraCondor }),
+  //   serviceContext: serviceContextFromDefaults({ }),
+  // });
 
-  // const index = await VectorStoreIndex.fromVectorStore(
-  //   vectorStore,
-  //   serviceContextFromDefaults({
-  //     llm: llm,
-  //     embedModel: llm,
-  //   })
-  // );
-  const response = await index
-    .asRetriever()
-    .retrieve(
-      "Napkins"
+  const index = await VectorStoreIndex.fromVectorStore(
+    astraCondor,
+    serviceContextFromDefaults()
+  );
+  const retreiver = index.asRetriever({nodePostprocessors:[
+    new MySimilarity({
+      similarityCutoff: 0.9,
+    })
+  ]})
+  
+  const [{score: a}, {score: b}] = await retreiver.retrieve(
+      "guns, germs, and steel"
     );
 
   // Output response
-  console.log(response);
+  console.log(a, b);
 }
 
 main().catch(console.error);
